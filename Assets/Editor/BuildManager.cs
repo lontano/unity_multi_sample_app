75c3f1609873f07717dc715689b2ae1bf186e232
lontano <ggeelloo@gmail.com>
2022-08-29 00:46:37 +0200

    Added build script

---
+++ Assets/Editor/BuildManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    [MenuItem("Tools/Build/Build Android")]
+    public static void BuildAndroid()
+  {
+    BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleSCene.unity" };
+    buildPlayerOptions.locationPathName = "Build/unity_multi_sample.apk";
+    buildPlayerOptions.target = BuildTarget.Android;
+    buildPlayerOptions.options = BuildOptions.None;
+
+    BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    BuildSummary summary = report.summary;
+
+    if (summary.result == BuildResult.Succeeded)
+    {
+      Debug.Log(message: "Build succeded: " + summary.totalSize + " bytes");
+    }
+    if (summary.result == BuildResult.Failed)
+    {
+      Debug.Log(message: "Build failed");
+    }
+  }
+}
